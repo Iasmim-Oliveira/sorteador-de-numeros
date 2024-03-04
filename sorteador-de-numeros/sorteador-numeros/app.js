dd5576923a75879e016fb6cc6160c091dfada659
Iasmim Oliveira <iasmimoliveiraestudos@gmail.com>
2024-03-04 19:19:28 -0300

    finish project - feito borao reiniciar e alerta

--- sorteador-de-numeros/sorteador-numeros/app.js
+++ sorteador-de-numeros/sorteador-numeros/app.js
@@ -6,14 +6,27 @@ function sortear () {
     // let resultado = numeroSorteado (de, ate);
     // alert(resultado);
 
+    if (de > ate) {
+        Swal.fire("O número inicial não pode ser maior que o final!");
+    }
+
     let sorteados = [];
     let numero;
 
     for (let i = 0; i < quantidade; i++){ //i é a variável; enquanto i < que quantidade, encrementa + 1. vai de 0 até quantidade.
         numero = numeroSorteado (de, ate); //função que vai ser executada de acordo com a quantidade
+
+        while (sorteados.includes(numero)) {//enquanto o array tiver o numero sorteado, repetir a função
+        numero = numeroSorteado (de, ate);
+        }
+
         sorteados.push(numero); //guarda os numeros sorteados no array
     }
-    alert(sorteados);
+
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: <span class="container__numero-sorteado"> ${sorteados}</span></label>`; 
+    //innerHTML insere uma tag no documento HTML.
+    alterarStatusBotao ();
 }
 
 function numeroSorteado (min, max) { //precisa passar o paramento, que são os valores "de" e "ate"
@@ -22,6 +35,21 @@ function numeroSorteado (min, max) { //precisa passar o paramento, que são os v
     return Math.floor(Math.random() * (max - min + 1)) + min; //+1 para incluir o último número
 }
 
-function reiniciar (){
+function alterarStatusBotao (){
+    let botao = document.getElementById('btn-reiniciar');
+
+    if (botao.classList.contains('container__botao-desabilitado')) {
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+
+    } else {
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
+
+}
+
+function reiniciar () {
+    window.location.reload(true);
 
 }
